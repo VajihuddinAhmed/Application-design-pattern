86a635be635e7ed8a1a04c111800ec4a063d159c
VajihuddinAhmed <vajihahmed@gmail.com>
2020-11-16 02:36:08 +0100

    working on js file

--- index.js
+++ index.js
@@ -5,13 +5,42 @@ const fetchData = async function (searchTerm) {
             s : searchTerm
         }
     })
-    console.log(response.data)
+
+    if (response.data.Error) {
+        return []
+    }
+
+
+    return response.data.Search
 }
 
-const input = document.querySelector('input')
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
 
-const onInput = (e) => {
-    fetchData(e.target.value)
+`
+
+const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const results = document.querySelector('.results')
+
+const onInput = async (e) => {
+    const movies = await fetchData(e.target.value)
+    for( let mov of movies) {
+        const div = document.createElement('div')
+
+        div.innerHTML = `
+        <img src=${mov.Poster} />
+        <h1>${mov.Title}</h1>
+        `
+        document.querySelector('#target').appendChild(div)
+    }
 }
 
 input.addEventListener('input', debounce(onInput, 500))
