bf0481f19a300ef7cd94054d5f0b320664978789
VajihuddinAhmed <vajihahmed@gmail.com>
2020-11-17 19:05:20 +0100

    project under construction

--- index.js
+++ index.js
@@ -1,46 +1,75 @@
-const fetchData = async function (searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params : {
-            apikey : '10db1a79',
-            s : searchTerm
-        }
-    })
-
-    if (response.data.Error) {
-        return []
-    }
-
-
-    return response.data.Search
-}
-
-const root = document.querySelector('.autocomplete')
-root.innerHTML = `
-    <label><b>Search For a Movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-
-`
-
-const input = document.querySelector('input')
-const dropdown = document.querySelector('.dropdown')
-const results = document.querySelector('.results')
-
-const onInput = async (e) => {
-    const movies = await fetchData(e.target.value)
-    for( let mov of movies) {
-        const div = document.createElement('div')
-
-        div.innerHTML = `
-        <img src=${mov.Poster} />
-        <h1>${mov.Title}</h1>
+const autoCompleteConfig = {
+    renderOption(movie) {
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+        return `
+            <img src=${imgSrc} />
+            ${movie.Title} (${movie.Year})
         `
-        document.querySelector('#target').appendChild(div)
+    },
+    inputValue(movie) {
+        return movie.Title
+    },
+    async fetchData (searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params : {
+                apikey : '10db1a79',
+                s : searchTerm
+            }
+        })
+    
+        if (response.data.Error) {
+            return []
+        }
+    
+    
+        return response.data.Search
     }
+
 }
 
-input.addEventListener('input', debounce(onInput, 500))
+
+
+const movieTemplate = (movieDetail) => {
+    
+    const metascore = parseInt(movieDetail.Metascore)
+    const imdbRating = parseFloat(movieDetail.imdbRating)
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''))
+    
+    
+    return ` 
+    <article class="media">
+        <figure class="media-left">
+            <p class="image">
+                <img src="${movieDetail.Poster}" />
+            </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+            </div>
+        </div>
+    </article>
+    <article class="notification is-primary">
+        <p class="title">${movieDetail.Awards}</p>
+        <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+        <p class="title">${movieDetail.BoxOffice}</p>
+        <p class="subtitle">Box Office</p>
+    </article>
+    <article data-value=${metascore} class="notification is-primary">
+        <p class="title">${movieDetail.Metascore}</p>
+        <p class="subtitle">Metascore</p>
+    </article>
+    <article data-value=${imdbRating} class="notification is-primary">
+        <p class="title">${movieDetail.imdbRating}</p>
+        <p class="subtitle">IMDB Ratings</p>
+    </article>
+    <article data-value=${imdbVotes} class="notification is-primary">
+        <p class="title">${movieDetail.imdbVotes}</p>
+        <p class="subtitle">IMDB Votes</p>
+    </article>
+    `
+}
